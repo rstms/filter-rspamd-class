64246927853a43970403a91ab96dbdf696e85468
Matt Krueger <mkrueger@rstms.net>
2024-11-03 06:50:14 -0700

    update rspamd-classes version

--- filter_rspamd_class_test.go
+++ filter_rspamd_class_test.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"testing"
-	"github.com/stretchr/testify/require"
 	"github.com/rstms/rspamd-classes/classes"
-    )
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 func TestVersion(t *testing.T) {
-    SpamClasses, err := classes.New("")
+	SpamClasses, err := classes.New("")
 	require.Nil(t, err)
 	require.NotEmpty(t, classes.Version)
 	require.NotNil(t, SpamClasses)
--- go.mod
+++ go.mod
@@ -4,12 +4,12 @@ go 1.22.5
 
 require (
 	github.com/poolpOrg/OpenSMTPD-framework v0.1.9
-	github.com/rstms/rspamd-classes v0.0.9
+	github.com/rstms/rspamd-classes v0.2.1
+	github.com/stretchr/testify v1.9.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.9.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
